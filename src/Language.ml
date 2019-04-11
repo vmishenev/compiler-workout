c16091d2b501764c20269badaee101b46c848b5a
vmishenev <vad-mishenev@yandex.ru>
2019-04-11 17:58:47 +0300

    add language.ml

--- src/Language.ml
+++ src/Language.ml
@@ -74,7 +74,40 @@ module Expr =
        which takes an environment (of the same type), a name of the function, a list of actual parameters and a configuration, 
        an returns resulting configuration
     *)                                                       
-    let rec eval env ((st, i, o, r) as conf) expr = failwith "Not implemented"
+    let to_int b = if b then 1 else 0
+    let to_bool x = x != 0
+                                                      
+    let to_func op =
+      let bti   = function true -> 1 | _ -> 0 in
+      let itb b = b <> 0 in
+      let (|>) f g   = fun x y -> f (g x y) in
+      match op with
+      | "+"  -> (+)
+      | "-"  -> (-)
+      | "*"  -> ( * )
+      | "/"  -> (/)
+      | "%"  -> (mod)
+      | "<"  -> bti |> (< )
+      | "<=" -> bti |> (<=)
+      | ">"  -> bti |> (> )
+      | ">=" -> bti |> (>=)
+      | "==" -> bti |> (= )
+      | "!=" -> bti |> (<>)
+      | "&&" -> fun x y -> bti (itb x && itb y)
+      | "!!" -> fun x y -> bti (itb x || itb y)
+      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)    
+    
+    let rec eval env ((st, i, o, r) as cfg) expr =      
+      match expr with
+      | Const n -> (s, i, o, Some n)
+      | Var   x -> (st, i, o, Some (State.eval st x))
+      | Binop (op, x, y) ->  let ((_, _, _, Some x') as cfg') = eval env cfg x in
+         let (st', i', o', Some y') = eval env cfg' y in
+         (st', i', o', Some (to_func op a b))
+      | Call (name, args) ->
+         let ((st', i', o', args) as cfg') = List.fold_left (fun (s, i, o, args) arg ->
+			let (s, i, o, Some res) = eval env (s, i, o, None) arg in 
+                         (s, i, o, args @ [res])) (s, i, o, []) args in env#definition env name args (st', i', o', None)
          
     (* Expression parser. You can use the following terminals:
 
@@ -82,8 +115,30 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string                                                                                                                  
     *)
     ostap (                                      
-      parse: empty {failwith "Not implemented"}
+      expr:
+	  !(Ostap.Util.expr 
+             (fun x -> x)
+	     (Array.map (fun (a, s) -> a, 
+                           List.map  (fun s -> ostap(- $(s)), (fun x y -> Binop (s, x, y))) s
+                        ) 
+              [|                
+		`Lefta, ["!!"];
+		`Lefta, ["&&"];
+		`Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+		`Lefta, ["+" ; "-"];
+		`Lefta, ["*" ; "/"; "%"];
+              |] 
+	     )
+	     primary);
+      
+      primary:
+        n:DECIMAL {Const n}
+      | x:IDENT   {Var x}
+      | name:IDENT "(" args:(!(Expr.expr))* ")" {Call (name, args)};
+      | fun_name:IDENT -"(" fun_args:!(Util.list0)[parse] -")" { Call (fun_name, fun_args) }
+      | -"(" expr -")"
     )
+    
     
   end
                     
@@ -100,7 +155,7 @@ module Stmt =
     (* empty statement                  *) | Skip
     (* conditional                      *) | If     of Expr.t * t * t
     (* loop with a pre-condition        *) | While  of Expr.t * t
-    (* loop with a post-condition       *) | Repeat of t * Expr.t
+    (* loop with a post-condition       *) | RepeatUntil of t * Expr.t
     (* return statement                 *) | Return of Expr.t option
     (* call a procedure                 *) | Call   of string * Expr.t list with show
                                                                     
@@ -111,12 +166,84 @@ module Stmt =
        Takes an environment, a configuration and a statement, and returns another configuration. The 
        environment is the same as for expressions
     *)
-    let rec eval env ((st, i, o, r) as conf) k stmt = failwith "Not implemnted"
+    let meta_op k s =
+        match k with
+        | Skip -> s
+        | _ -> Seq (s, k)
+
+    let rec eval env ((s, i, o, _) as cfg) k stmt = match stmt with
+	| Read       v  -> let head::tail = i in
+              (State.update v head s, tail, o, None) Skip k
+        | Write      e  -> (s, i, o @ [Expr.eval s e], None) 
+        | Assign (v, e) -> let (s, i, o, Some r) = Expr.eval env cfg e in
+              eval env (State.update v r s, i, o, Some r) Skip k
+        | Seq (prev, next) ->  eval env cfg (meta_op k next) prev
+        | Skip -> match k with
+              | Skip -> cfg
+              | _ -> eval env cfg Skip k
+        | If(cond, then_branch, else_branch) ->
+              let ((s, i, o, Some cond_res) as cfg') = Expr.eval env cfg cond in
+              eval env cfg' k (if Expr.to_bool (cond_res) then  then_branch else else_branch) 
+        | While (e, body) ->
+              let ((s, i, o, Some e_res) as cfg') = Expr.e env cfg cond in
+              if Expr.to_bool (e_res) then eval env cfg' (meta_op k stmt) body else 
+              eval env (s, i, o, None) Skip k
+        | RepeatUntil (body, e) ->
+              eval env cfg (meta k (While (Expr.Binop ("==", e, Expr.Const 0), body))) body
+        | Call (name, args) ->
+              (*cfg across expr*)
+              (eval env (Expr.eval env cfg (Expr.Call (name, args))) Skip k
+              (*let (arg_names, local_vars, body) = env#definition name in
+              let local_state = State.push_scope s (arg_names @ local_vars) in
+	      let bindings = List.combine arg_names (List.map (Expr.eval s) args) in
+              let local_state_init =
+                    List.fold_right(fun (arg_name, arg_value) s' -> State.update arg_name arg_value s')
+                    bindings local_state
+             in
+             let (end_state, i, o) = eval env (local_state_init, i, o) body in
+            (State.drop_scope end_state s, i, o)*)
+        | Return res -> match res with
+           | Some res -> Expr.eval env cfg res
+           | _ -> (st, i, o, None)
+         ;;
+
          
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented"}
-    )
+      simple_stmt:
+                  x:IDENT ":=" e:!(Expr.expr)       {Assign(x, e)}
+               | "read" "(" x:IDENT ")"           {Read x}
+               | "write" "(" e:!(Expr.expr) ")" {Write e};
+
+      construct_stmt:
+           "while" cond:!(Expr.expr) "do" body:!(parse) "od" 
+            { While (cond, body)}
+           | "for" init:!(parse) "," cond:!(Expr.expr) "," action:!(parse) "do" body:!(parse) "od"
+            {  Seq(init, While(cond, Seq(body, action))) }
+           | "repeat" body:!(parse) "until" cond:!(Expr.expr)
+            { RepeatUntil (body, cond)}
+           | "if" cond:!(Expr.expr)
+                "then" th:!(parse)
+                elif_branch: (%"elif" !(Expr.expr) %"then" !(parse))*
+                else_branch:  (%"else" !(parse))?
+                "fi"
+            {
+                    let else_branch' = match else_branch with
+                        | Some x -> x
+                        | _ -> Skip
+                    in
+                    let elif_branch' = List.fold_right (fun (cond, body) t -> If (cond, body, t)) elif_branch else_branch' in
+                    If (cond, th, elif_branch')
+            }
+           | "skip" {Skip};
+
+      call_stmt: name:IDENT "(" args:(!(Expr.expr))* ")" {Call (name, args)};
+     
+      stmt:    simple_stmt | construct_stmt | call_stmt;
+
+      parse:     line:stmt ";" rest:parse      {Seq(line, rest)} 
+	       | stmt
+      )
       
   end
 
@@ -127,8 +254,14 @@ module Definition =
     (* The type for a definition: name, argument list, local variables, body *)
     type t = string * (string list * string list * Stmt.t)
 
-    ostap (     
-      parse: empty {failwith "Not implemented"}
+    ostap (                                      
+         parse: "fun" name:IDENT "(" args:(IDENT)* ")" local_vars:(%"local" (IDENT)*)? "{" body:!(Stmt.parse) "}"
+          {
+              let local_vars_list = match local_vars with
+              | Some x -> x
+              | _ -> [] (*empty*) in 
+              name, (args, local_vars_list, body)
+          }
     )
 
   end
